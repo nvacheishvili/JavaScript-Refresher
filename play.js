ee88b8e974e8dc90873b2cced501314b57ae894c
Nata Vacheishvili <natashkela@gmail.com>
2023-04-23 21:47:12 -0400

    Arrow Functions

--- play.js
+++ play.js
@@ -1,38 +1,36 @@
-//How to declare a variable
-//var is an outdated way to declare a variable, from now on we are going to be using let
-
-//If a variable's value should never change make it constant (const) so that you get an error if you accidentally change it
-const name = "Nata"; //example of string that cannot be changed because it is a constant
-
-let age = 26; //example of number
-let userHobbies = true; //example of boolean
-
-//Run this file in your terminal to see outputted result by console.log
-//It should output: Nata
-console.log(name, age, userHobbies);
-
-
-let nameAgeCombination = name+age; //Global Variable
-
-//If you uncomment the line below, you are going to get an error when running this file stating that you are assigning
-// a value to constant variable
-
-// name="Nata Vacheishvili"
-
-//How to declare a function
-/***
- *
- * @param name - Name of the user
- * @param age - Age of the user
- * @returns {string} - would return a string such as Name is Nata, Age is 26
+/**
+ * Example of function that sums a and b arguments using the traditional function definition
+ * @param a - expects a number
+ * @param b - expects a number
+ * @returns {number} returns the sum of a and b
  */
-function whoIsUser(name, age){
-    /* console.log(nameAgeCombination) - This will be accessible since the variable is defined outside and would console log Nata26
-       but if you were to define nameAgeCombination variable here instead and tried to access it outside of this function
-       due to function scoping, this variable would be undefined.
-     */
-    return "Name is "+name+", Age is "+age;
-}
-// See the output of the function with given arguments of name and age
-console.log(whoIsUser(name, age));
 
+function myFunction(a,b) {
+    return a+b;
+}
+
+/**
+ * Example of a function that sums a and b arguments using the arrow function definition ES6 Syntax
+ * @param a - expects a number
+ * @param b - expects a number
+ * @returns {number} - returns the sum of a and b
+ */
+const myArrowFunction = (a,b) => {
+    //Your other logic before returning a value would go here...
+    return a+b;
+}
+
+//You can also rewrite the ^^^^ above function in a shorter way
+//Only do this if you have a single statement that would return a value, if you have multiple statements
+//that do some other actions inside the function that do not return the value right away, then do not do this.
+// const myArrowFunction = (a,b) => a+b;
+
+/**
+ * Function that returns 10 directly
+ * @returns {number}
+ */
+const random = () => 10;
+
+
+//Running this file should give you the same results for myFunction and myArrowFunction if you pass the same arguments to each one of them
+console.log(myFunction(1,2), myArrowFunction(1,2), random());
